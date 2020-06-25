b7e3f18cdf9a7a9b85d104aa221665a351aa874d
abramepithey <abramepithey@gmail.com>
2020-06-25 17:56:08 -0400

    Adds Note Create and Note Navlink

--- ElevenNote2.MVC/Controllers/NoteController.cs
+++ ElevenNote2.MVC/Controllers/NoteController.cs
@@ -16,5 +16,23 @@ namespace ElevenNote2.MVC.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        // GET: Note/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Note/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
