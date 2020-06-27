f8beec4dce2de4e338ad9620c64e3a302d29407d
abramepithey <abramepithey@gmail.com>
2020-06-27 10:06:32 -0400

    Adds Note delete view, and controller and service methods

--- ElevenNote2.MVC/Controllers/NoteController.cs
+++ ElevenNote2.MVC/Controllers/NoteController.cs
@@ -103,6 +103,31 @@ namespace ElevenNote2.MVC.Controllers
             return View();
         }
 
+        // GET: Note/Delete/{id}
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
+        // POST: Note/Delete/{id}
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateNoteService();
+
+            service.DeleteNote(id);
+
+            TempData["SaveResult"] = "Your note was deleted.";
+
+            return RedirectToAction("Index");
+        }
+
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
